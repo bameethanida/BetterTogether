37918fd1ef46b1c715196e65c23bd84755316b19
bleachjade <bleachjade@hotmail.com>
2019-11-13 20:43:16 +0700

    user can change profile img

--- BetterTogetherApp/templates/BetterTogetherApp/js/app.js
+++ BetterTogetherApp/templates/BetterTogetherApp/js/app.js
@@ -9,3 +9,19 @@ function pasuser(form) {
       alert("Invalid UserID")
     }
   }
+
+
+  $("#profileImage").click(function(e) {
+    $("#imageUpload").click();
+});
+
+function fasterPreview( uploader ) {
+    if ( uploader.files && uploader.files[0] ){
+          $('#profileImage').attr('src', 
+             window.URL.createObjectURL(uploader.files[0]) );
+    }
+}
+
+$("#imageUpload").change(function(){
+    fasterPreview( this );
+});
