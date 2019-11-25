66212c1c2eb09047d669c8eb050c7892ca1325b2
bameethanida <bameejrn@outlook.com>
2019-11-25 16:07:35 +0700

    minor changes

--- BetterTogetherApp/static/js/profile_script.js
+++ BetterTogetherApp/static/js/profile_script.js
@@ -1,27 +1,14 @@
-function pasuser(form) {
-    if (form.id.value == "1") {
-      if (form.pass.value == "2") {
-        location = "profile.html"
-      } else {
-        alert("Invalid Password")
-      }
-    } else {
-      alert("Invalid UserID")
+  $("#profileImage").click(function (e) {
+    $("#imageUpload").click();
+  });
+
+  function fasterPreview(uploader) {
+    if (uploader.files && uploader.files[0]) {
+      $('#profileImage').attr('src',
+        window.URL.createObjectURL(uploader.files[0]));
     }
   }
 
-
-  $("#profileImage").click(function(e) {
-    $("#imageUpload").click();
-});
-
-function fasterPreview( uploader ) {
-    if ( uploader.files && uploader.files[0] ){
-          $('#profileImage').attr('src', 
-             window.URL.createObjectURL(uploader.files[0]) );
-    }
-}
-
-$("#imageUpload").change(function(){
-    fasterPreview( this );
-});
+  $("#imageUpload").change(function () {
+    fasterPreview(this);
+  });
